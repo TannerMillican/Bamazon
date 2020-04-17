36e0c8ae9412dea3a7be29c1f206f80c741fec93
TannerMillican <tannermillican@gmail.com>
2020-04-17 16:48:29 -0500

    got inquirer to function as expected

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -10,16 +11,18 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+    // console.log("connected as id " + connection.threadId);
     afterConnection()
 });
 
 function afterConnection(){
     connection.query("SELECT * FROM products", function(err, res){
         if(err) throw err;
-        console.log(res)
+        // console.log(res)
 
         var items = [];
+        var itemsInfo = [];
+        var itemIDs = [];
 
         console.log("            Item ID        Product           Department       Price    Stock");
         console.log("----------------------------------------------------------------------------")
@@ -28,17 +31,49 @@ function afterConnection(){
             var item = [];
             var itemID = res[i].item_id;
             item.push(itemID);
+            itemIDs.push(itemID)
+            itemsInfo.push(itemID);
             var productName = res[i].product_name;
             item.push(productName);
+            itemsInfo.push(productName);
             var departmentName = res[i].department_name;
             item.push(departmentName);
+            itemsInfo.push(departmentName);
             var itemPrice = res[i].price;
             item.push(itemPrice);
+            itemsInfo.push(itemPrice);
             var itemQuantity = res[i].stock_quantity;
             item.push(itemQuantity);
+            itemsInfo.push(itemQuantity);
             items.push(item)
         }
         console.table(items)
+        inquire()
 
+        function inquire(){
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: "What is the ID of the Product you would like to purchase?",
+                        name: "itemChoice"
+                    },
+                    {
+                        type: "number",
+                        message: "How many would you like to purchase?",
+                        name: "purchaseNumber"
+                    }
+                ])
+                .then(function(inquirerResponse){
+                    for (var i = 0; i < items.length; i++){
+                        if (inquirerResponse.itemChoice = items[i] && inquirerResponse.purchaseNumber < items[i].slice(4)) {
+                            // updateProduct()
+                        } else {
+                            console.log("Sorry, there's currently not that many items stocked. Please enter a lower amount.")
+                            inquire()
+                        }
+                    }
+                })
+            }
     })
 }
